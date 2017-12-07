034bcd57724e9ec27791d063195046afbf33494c
Jarrod Quan <jarrodquan@gmail.com>
2017-12-07 23:21:24 +0800

    compress run-time image

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 rd /q /s greetings
-jlink -p "%JAVA_HOME%\jmods;mlib" --add-modules com.greetings,com.astro --launcher greetings=com.greetings --output greetings
+jlink -p "%JAVA_HOME%\jmods;mlib" --add-modules com.greetings,com.astro --launcher greetings=com.greetings --output greetings --compress 2 --strip-debug
