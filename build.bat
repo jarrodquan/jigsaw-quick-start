5187ea68d5957f8e14232c013c6792726d09ae8c
JarrodQuan <jarrodquan@gmail.com>
2017-12-08 13:49:31 +0800

    add javapackager.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-rd /q /s greetings
-jlink -p "%JAVA_HOME%\jmods;mlib" --add-modules com.greetings,com.astro --launcher greetings=com.greetings --output greetings --compress 2 --strip-debug
+rd /q /s greetings-jre
+jlink -p "%JAVA_HOME%\jmods;mlib" --add-modules com.greetings,com.astro --launcher greetings=com.greetings --output greetings-jre --compress 2 --strip-debug
---
+++ javapackager.bat
@@ -0,0 +1,1 @@
+javapackager -deploy -outdir greetings-native -outfile greetings-native -srcdir src -native image -appclass com.greetings.Main -Bruntime=greetings-jre
